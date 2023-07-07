5df2415a5a6fe12e9df6757ef1acae5dfeb55789
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-07-07 10:25:17 +0100

    DEV: set up quality tools

---
+++ .commitlint.rules.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    "header-max-length": [2, "always", 65],
+    "subject-case": [0, "always", "sentence-case"],
+    "scope-enum": [2, "always", ["catalog", "covariance", "io", "maps", "plot",
+                                 "twopoint", "util"]],
+    "scope-case": [0, "always", "lower-case"],
+    "type-enum": [2, "always", ["API", "BUG", "DEP", "DEV", "DOC", "ENH",
+                                "MNT", "REV", "STY", "TST", "TYP", "REL"]],
+    "type-case": [0, "always", "upper-case"],
+  }
+}
