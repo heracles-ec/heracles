ab0a5441275eec4c7cdaa2b62cc3947127bd7f10
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-09-12 11:48:59 +0100

    remove duplicated test

--- tests/test_core.py
+++ tests/test_core.py
@@ -78,33 +78,3 @@ def test_tocdict():
     d = TocDict(a=1) | TocDict(b=2)
     assert type(d) is TocDict
     assert d == {"a": 1, "b": 2}
-
-
-def test_progress():
-    from io import StringIO
-
-    from heracles.util import Progress
-
-    f = StringIO()
-    prog = Progress(f)
-    prog.start(10, "my title")
-    s = f.getvalue()
-    assert s.count("\r") == 1
-    assert s.count("\n") == 0
-    assert "my title" in s
-    assert "0/10" in s
-    prog.update()
-    s = f.getvalue()
-    assert s.count("\r") == 2
-    assert s.count("\n") == 0
-    assert "1/10" in s
-    prog.update(5)
-    s = f.getvalue()
-    assert s.count("\r") == 3
-    assert s.count("\n") == 0
-    assert "6/10" in s
-    prog.stop()
-    s = f.getvalue()
-    assert s.count("\r") == 4
-    assert s.count("\n") == 1
-    assert "10/10" in s
