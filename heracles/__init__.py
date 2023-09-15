93b61ee2ae6ed062449022c7a184adc4f7581ade
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-09-15 15:24:35 +0100

    DEV: Improve logging

--- heracles/__init__.py
+++ heracles/__init__.py
@@ -17,7 +17,21 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with Heracles. If not, see <https://www.gnu.org/licenses/>.
 
+import logging
+
 try:
     from ._version import __version__, __version_tuple__  # noqa: F401
 except ModuleNotFoundError:
     pass
+
+_logger = logging.getLogger(__name__)
+
+_formatter = logging.Formatter(
+    "%(levelname)s [%(asctime)s] sleplet: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+_console_handler = logging.StreamHandler()
+_console_handler.setFormatter(_formatter)
+_logger.addHandler(_console_handler)
+_logger.setLevel("INFO")
+_logger.propagate = False
