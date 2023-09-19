c7fd081ed51661b2cee295dab7068c349dde0150
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-09-19 14:55:34 +0100

    separate structural and content changes

--- heracles/__init__.py
+++ heracles/__init__.py
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with Heracles. If not, see <https://www.gnu.org/licenses/>.
+"""Main module of the *Heracles* package."""
 
 try:
     from ._version import __version__, __version_tuple__  # noqa: F401
