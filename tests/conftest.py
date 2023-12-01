ff763e7aaf7a861c5b8c0f13f8ee4971768db1a8
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-12-01 08:50:19 +0000

    remove unnecessary test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,8 @@
-import warnings
-from contextlib import contextmanager
-
 import numpy as np
 import pytest
 from numba import config
 
 config.DISABLE_JIT = True
-
-
-@contextmanager
-def warns(*types):
-    if types == (None,):
-        with warnings.catch_warnings():
-            warnings.simplefilter("error")
-            try:
-                yield
-            finally:
-                pass
-    else:
-        with pytest.warns(*types):
-            try:
-                yield
-            finally:
-                pass
 
 
 @pytest.fixture(scope="session")
