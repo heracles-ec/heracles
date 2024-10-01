5669e3e1ba7dc28d9a1bcce3b878acaffa20a4b7
jaimerzp <jaimerz011235813@gmail.com>
2024-10-01 15:20:11 +0300

    bug

--- heracles/catalog/filters.py
+++ heracles/catalog/filters.py
@@ -91,7 +91,7 @@ class FootprintFilter:
         from healpy import ang2pix
 
         lon, lat = self._lonlat
-        good_entry = np.isfiniten(lon) & np.isfinite(lat)
+        good_entry = np.isfinite(lon) & np.isfinite(lat)
         lon, lat = lon[good_entry], lat[good_entry]
         ipix = ang2pix(self._nside, page[lon], page[lat], lonlat=True)
         exclude = np.where(self._footprint[ipix] == 0)[0]
