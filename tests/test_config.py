b9842d5a22204f851e61b94936c86ab64b216970
Nicolas Tessore <n.tessore@ucl.ac.uk>
2024-11-24 13:13:57 +0000

    wip configuration format

---
+++ tests/test_config.py
@@ -0,0 +1,127 @@
+import io
+import textwrap
+import unittest.mock
+
+import pytest
+
+import heracles
+import heracles.config
+
+
+def load(text):
+    with io.StringIO(textwrap.dedent(text)) as stream:
+        loader = heracles.config.YamlLoader(stream)
+        try:
+            return loader.get_single_data()
+        finally:
+            loader.dispose()
+
+
+@unittest.mock.patch("heracles.Positions")
+def test_field_construction(mock):
+    field = load(
+        """
+        !heracles.Positions
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None)
+
+    field = load(
+        """
+        !heracles.Positions lon lat weight
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None, "lon", "lat", "weight")
+
+    field = load(
+        """
+        !heracles.Positions
+          - lon
+          - lat
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None, "lon", "lat")
+
+    field = load(
+        """
+        !heracles.Positions
+          mapper: xyz
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with("xyz")
+
+    field = load(
+        """
+        !heracles.Positions
+          columns: lon lat
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None, "lon", "lat")
+
+    field = load(
+        """
+        !heracles.Positions
+          columns:
+            - lon
+            - lat
+            - weight
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None, "lon", "lat", "weight")
+
+    field = load(
+        """
+        !heracles.Positions
+          arg1: abc
+          arg2: 2
+          arg3:
+            - x
+            - y
+            - z
+        """
+    )
+    assert field is mock.return_value
+    mock.assert_called_with(None, arg1="abc", arg2=2, arg3=["x", "y", "z"])
+
+
+@pytest.mark.parametrize(
+    "field_type",
+    [
+        "heracles.Positions",
+        "heracles.Shears",
+        "heracles.Visibility",
+        "heracles.Weights",
+    ],
+)
+def test_field_type(field_type):
+    with unittest.mock.patch(field_type) as mock:
+        field = load(f"!{field_type}")
+        assert field is mock.return_value
+        mock.assert_called_with(None)
+
+
+@unittest.mock.patch("heracles.config.YamlLoader")
+def test_load_config(mock, tmp_path):
+    path = tmp_path / "test.yaml"
+    path.touch()
+
+    values = {"a": 1, "b": 2, "c": 3}
+
+    mock.return_value.get_single_data.return_value = values
+
+    config = heracles.load_config(path)
+
+    assert mock.call_count == 1
+    assert mock.call_args[0][0].name == str(path)
+
+    loader = mock.return_value
+    loader.get_single_data.assert_called_once_with()
+    loader.dispose.assert_called_once_with()
+
+    assert config.__dict__ == values
