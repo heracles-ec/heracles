71c71f7244fe26ee3906f41f0e1f25dc4fc99210
jaimerzp <jaimerz011235813@gmail.com>
2025-03-05 18:46:04 +0000

    bug?

--- heracles/dices/utils_polspice.py
+++ heracles/dices/utils_polspice.py
@@ -20,7 +20,7 @@ import numpy as np
 from scipy.special import lpn as legendrep
 
 try:
-    from .mathutils import gauss_legendre
+    from ..mathutils import gauss_legendre
 except:
     # use np.polynomial.legendre if can't load fast native (so can use module without compiling camb)
     # Fortran version is much faster than current np.polynomial
