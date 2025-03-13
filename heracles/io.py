7d11acd63a01653283b1736613946a4e40130ab9
baugstein <87702063+ucapbba@users.noreply.github.com>
2025-03-13 10:10:43 +0000

    #247 FitsDict update to work with Chain Map (#248)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bradley Augstein <ucapbba@Bradleys-MacBook-Pro.local>
    Co-authored-by: Nicolas Tessore <n.tessore@ucl.ac.uk>

--- heracles/io.py
+++ heracles/io.py
@@ -618,6 +618,8 @@ class FitsDict(MutableMapping):
         data = self._cache.get(ext)
         if data is None:
             with self.fits as fits:
+                if ext not in fits:
+                    raise KeyError(ext)
                 data = self.reader(fits[ext])
             self._cache[ext] = data
         return data
