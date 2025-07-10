eb0b9241f9558e11df7cc0539821ab0e52b1b6a8
jaimerzp <jaimerz011235813@gmail.com>
2025-07-10 19:50:21 +0100

    found another bug

--- heracles/dices/jackknife.py
+++ heracles/dices/jackknife.py
@@ -298,6 +298,8 @@ def sample_covariance(samples, samples2=None):
         mu += delta / (i + 1)
         mu2 += (y - mu2) / (i + 1)
         cov += (np.multiply.outer(delta, y - mu2) - cov) / (i + 1)
+    # normalize covariance
+    cov *= n / (n - 1)
     return cov
 
 
