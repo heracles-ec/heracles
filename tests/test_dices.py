2eda37994ae3125f8225dabe5a534a0d7ac16120
jaimerzp <jaimerz011235813@gmail.com>
2025-10-28 11:14:01 +0000

    ruff

--- tests/test_dices.py
+++ tests/test_dices.py
@@ -80,6 +80,7 @@ def test_mask_correction(cls0, mls0):
 
 def test_polspice(cls0):
     from heracles.dices.utils import get_cl
+
     cls = np.array(
         [
             get_cl(("POS", "POS", 1, 1), cls0),
